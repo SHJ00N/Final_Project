1188001a8152e2742cd78b5b662f9206869c32c5
SHJ00N <148672733+SHJ00N@users.noreply.github.com>
2024-05-24 13:53:23 +0900

    랭킹기능 구현 완료

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,6 +6,7 @@ using System.IO;
 using UnityEngine.UI;
 using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
     public GameObject registerButtonObj;
     public GameObject playerInputFieldObj;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI[] playerRank;
     public TextMeshProUGUI[] rank_name;
     public TextMeshProUGUI[] rank_score;
     public TextMeshProUGUI result_score;
@@ -29,6 +31,7 @@ public class GameManager : MonoBehaviour
     private string filePath;
 
     private bool isRigister = false;
+    private bool gameEndWindowActive = false;
 
     private void Awake()
     {
@@ -45,6 +48,7 @@ public class GameManager : MonoBehaviour
         LoadScores();
 
         nameInputField.characterLimit = 3;
+
     }
     // Update is called once per frame
     void Update()
@@ -59,12 +63,14 @@ public class GameManager : MonoBehaviour
             {
                 registerButtonObj.SetActive(true);
             }
-            Invoke("gameEndActive", 2f);
+            if(!gameEndWindowActive)
+                Invoke("gameEndActive", 2f);
         }
     }
 
     private void gameEndActive()
     {
+        gameEndWindowActive = true;
         gameEndSet.SetActive(true);
     }
 
@@ -91,6 +97,7 @@ public class GameManager : MonoBehaviour
                     }
                 }
             }
+            UpdateRanking();
         }
         else
         {
@@ -106,9 +113,21 @@ public class GameManager : MonoBehaviour
 
         int rank = 0;
         foreach(var entry in sortedRanking)
-        {
+        { 
             rank_name[rank].text = entry.Key;
             rank_score[rank].text = string.Format("{0:N0}", entry.Value);
+            if(entry.Key == nameInputField.text && isRigister)
+            {
+                playerRank[rank].color = Color.yellow;
+                rank_name[rank].color = Color.yellow;
+                rank_score[rank].color = Color.yellow;
+            }
+            else
+            {
+                playerRank[rank].color = Color.black;
+                rank_name[rank].color = Color.black;
+                rank_score[rank].color = Color.black;
+            }
             rank++;
 
             if (rank > 9) break;
@@ -117,30 +136,36 @@ public class GameManager : MonoBehaviour
 
     public void RegisterRanking()
     {
-        isRigister = true;
-        playerInputFieldObj.SetActive(false);
-        registerButtonObj.SetActive(false);
-        string playerName = nameInputField.text;
-
-        if (!string.IsNullOrEmpty(playerName))
+        if (!ranking.ContainsKey(nameInputField.text) && nameInputField.text != "Duplicate" && !string.IsNullOrEmpty(nameInputField.text))
         {
-            // 새로운 플레이어 점수를 랭킹에 추가
-            ranking[playerName] = score;
+            isRigister = true;
+            playerInputFieldObj.SetActive(false);
+            registerButtonObj.SetActive(false);
 
-            // 랭킹 업데이트
-            UpdateRanking();
+            string playerName = nameInputField.text;
+             // 새로운 플레이어 점수를 랭킹에 추가
+             ranking[playerName] = score;
 
-            // 랭킹 파일에 저장
-            SaveScores();
+             // 랭킹 업데이트
+             UpdateRanking();
+
+             // 랭킹 파일에 저장
+             SaveScores();
+        } else if (!string.IsNullOrEmpty(nameInputField.text))
+        {
+            nameInputField.text = "Duplicate";
         }
     }
 
     private void SaveScores()
     {
+        var sortedRanking = new List<KeyValuePair<string, int>>(ranking);
+        sortedRanking.Sort((x, y) => y.Value.CompareTo(x.Value));
+        Debug.Log("save!");
         using (StreamWriter writer = new StreamWriter(filePath))
         {
             int count = 0;
-            foreach (var entry in ranking)
+            foreach (var entry in sortedRanking)
             {
                 writer.WriteLine($"{entry.Key}:{entry.Value}");
                 count++;
@@ -165,6 +190,7 @@ public class GameManager : MonoBehaviour
     
     public void ActiveRegisterField()
     {
+        playerInputFieldObj.SetActive(true);
         playerInputFieldObj.SetActive(true);
     }
 
